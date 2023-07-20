5a09079e7131a65c172393bc3b282a38460a601b
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-07-20 12:14:27 -0500

    integrated firestore will style it later

--- src/Todos/AddTodo.jsx
+++ src/Todos/AddTodo.jsx
@@ -1,7 +1,45 @@
-import React, { useState } from 'react';
+import { addDoc, collection } from "firebase/firestore";
+import React, { useEffect, useState } from 'react';
+import { auth, db } from "../config/firebase";
 
-const AddTodo = () => {
+const AddTodo = ({ getTodos, onAddTodo }) => {
   const [todo, setTodo] = useState("");
+  const [content, setContent] = useState("");
+
+  const addTodo = async () => {
+    try {
+      if (todo.trim() === "" || content.trim() === "") {
+        return;
+      }
+
+      const todoRef = collection(db, "todos");
+      await addDoc(todoRef, {
+        title: todo,
+        content: content,
+        userId: auth?.currentUser?.uid
+      });
+
+      const newTodo = {
+        userId: auth?.currentUser?.uid,
+        title: todo,
+        content: content
+      };
+
+
+      if (onAddTodo) {
+        onAddTodo(newTodo);
+      }
+
+      setTodo("");
+      setContent("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
 
   return (
     <div>
@@ -10,7 +48,12 @@ const AddTodo = () => {
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
       />
-      <button>Add</button>
+      <input
+        type="text"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
     </div>
   );
 };
--- src/Todos/Todos.jsx
+++ src/Todos/Todos.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import { useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../config/firebase";
 import AddTodo from "./AddTodo";
 import DisplayTodo from "./DisplayTodo";
 
 const Todos = () => {
+  const [todos, setTodos] = useState([]);
+  const todosRfef = "todos";
+  const ref = collection(db, todosRfef);
+
+  const getTodos = async () => {
+    try {
+      const data = await getDocs(ref);
+      const filteredData = data.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+      }));
+      setTodos(filteredData);
+      console.log(todos);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleAddToTodo = (newTodo) => {
+    setTodos([newTodo, ...todos]);
+  };
 
   return (
     <div>
-      <AddTodo />
-      <DisplayTodo />
+      <AddTodo getTodos={getTodos} onAddTodo={handleAddToTodo} />
+      <DisplayTodo getTodos={getTodos} todos={todos} reference={ref} />
     </div>
   );
 };
